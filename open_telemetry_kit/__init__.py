39e47f56ec9b585b7e9bc4554eb4f2ed47af2563
Adam <adam@hivemapper.com>
2020-11-23 13:17:30 -0800

    Update init with new parsers

--- open_telemetry_kit/__init__.py
+++ open_telemetry_kit/__init__.py
@@ -1,8 +1,11 @@
 name = "open_telemetry_kit"
 from .element import Element
 from .parser import Parser
+
+from .assparser import ASSParser
+from .blackvueparser import BlackvueParser
 from .csvparser import CSVParser
-from .srtparser import SRTParser
-from .kmlparser import KMLParser
 from .gpxparser import GPXParser
 from .klvparser import KLVParser
+from .kmlparser import KMLParser
+from .srtparser import SRTParser
